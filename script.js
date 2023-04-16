e9c9ce406290ceec74e70e9695e296bce2625ee8
Hubert Muller <alotofgreatideas@gmail.com>
2023-04-16 15:15:42 +0200

    index

--- script.js
+++ script.js
@@ -6,3 +6,49 @@ var obiekt = {
 };
 
 var tablica = [ 1, "lancuch", false, [true, false, 1] ];
+
+var mojafunkcja = function (parametr1) { 
+    return parametr1*2 
+};
+
+var powiekszenie = function (e) {
+    var elementyFormularza = document.querySelectorAll("form input, form select, form textarea");
+    for(var x=0; x < elementyFormularza.length; x++) {
+
+        var biezacyElement = elementyFormularza[x];
+        //var styleElementu = window.getComputedStyle(biezacyElement);
+        var listaKlas = biezacyElement.classList;
+        listaKlas.remove("pomniejszony");
+        listaKlas.add("powiekszony");
+        //console.log(x, styleElementu.getPropertyValue('margin'), listaKlas);
+    }
+}
+
+var pomniejszenie = function (e) {
+    var elementyFormularza = document.querySelectorAll("form input, form select, form textarea");
+    for(var x=0; x < elementyFormularza.length; x++) {
+
+        var biezacyElement = elementyFormularza[x];
+        //var styleElementu = window.getComputedStyle(biezacyElement);
+        var listaKlas = biezacyElement.classList;
+        listaKlas.remove("powiekszony");
+        listaKlas.add("pomniejszony");
+        //console.log(x, styleElementu.getPropertyValue('margin'), listaKlas);
+    }
+}
+
+function maskowanie (e) {
+    document.querySelector("input[name='imie']").setAttribute("type", "password");
+}
+
+
+var elementyFormularza = document.querySelectorAll("form input");
+console.log("odnaleziono: " + elementyFormularza.length + " elementow input");
+
+var przyciskPow = document.querySelector("#powiekszenie");
+przyciskPow.addEventListener("click", powiekszenie);
+
+var przyciskPom = document.querySelector("#pomniejszenie");
+przyciskPom.addEventListener("click", pomniejszenie);
+
+document.querySelector("#maskuj").addEventListener("click", maskowanie);
