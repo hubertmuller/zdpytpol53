759d5887921ff17e5fc69b38805d34162366692e
Hubert Muller <alotofgreatideas@gmail.com>
2023-04-16 16:38:39 +0200

    forma

--- script.js
+++ script.js
@@ -1,54 +1,59 @@
-var obiekt = { 
-    imie: "Hubert", 
-    nazwisko: "Muller", 
-    wiek: 45, 
-    dzieci: ["jan", "ala"] 
-};
+(function () {
+    var obiekt = { 
+        imie: "Hubert", 
+        nazwisko: "Muller", 
+        wiek: 45, 
+        dzieci: ["jan", "ala"] 
+    };
 
-var tablica = [ 1, "lancuch", false, [true, false, 1] ];
+    let tablica = [ 1, "lancuch", false, [true, false, 1] ];
 
-var mojafunkcja = function (parametr1) { 
-    return parametr1*2 
-};
+    var mojafunkcja = function (parametr1) { 
+        return parametr1*2 
+    };
 
-var powiekszenie = function (e) {
-    var elementyFormularza = document.querySelectorAll("form input, form select, form textarea");
-    for(var x=0; x < elementyFormularza.length; x++) {
+    var powiekszenie = function (e) {
+        var elementyFormularza = document.querySelectorAll("form input, form select, form textarea");
+        for(let x=0; x < elementyFormularza.length; x++) {
 
-        var biezacyElement = elementyFormularza[x];
-        //var styleElementu = window.getComputedStyle(biezacyElement);
-        var listaKlas = biezacyElement.classList;
-        listaKlas.remove("pomniejszony");
-        listaKlas.add("powiekszony");
-        //console.log(x, styleElementu.getPropertyValue('margin'), listaKlas);
+            var biezacyElement = elementyFormularza[x];
+            //var styleElementu = window.getComputedStyle(biezacyElement);
+            var listaKlas = biezacyElement.classList;
+            listaKlas.remove("pomniejszony");
+            listaKlas.add("powiekszony");
+            //console.log(x, styleElementu.getPropertyValue('margin'), listaKlas);
+        }
+        //przy var - console.log(x); //10
+        //przy let - consol.log(x) - blad - undfined;
     }
-}
 
-var pomniejszenie = function (e) {
-    var elementyFormularza = document.querySelectorAll("form input, form select, form textarea");
-    for(var x=0; x < elementyFormularza.length; x++) {
+    var pomniejszenie = function (e) {
+        var elementyFormularza = document.querySelectorAll("form input, form select, form textarea");
+        for(let x=0; x < elementyFormularza.length; x++) {
 
-        var biezacyElement = elementyFormularza[x];
-        //var styleElementu = window.getComputedStyle(biezacyElement);
-        var listaKlas = biezacyElement.classList;
-        listaKlas.remove("powiekszony");
-        listaKlas.add("pomniejszony");
-        //console.log(x, styleElementu.getPropertyValue('margin'), listaKlas);
+            var biezacyElement = elementyFormularza[x];
+            //var styleElementu = window.getComputedStyle(biezacyElement);
+            var listaKlas = biezacyElement.classList;
+            listaKlas.remove("powiekszony");
+            listaKlas.add("pomniejszony");
+            //console.log(x, styleElementu.getPropertyValue('margin'), listaKlas);
+        }
     }
-}
 
-function maskowanie (e) {
-    document.querySelector("input[name='imie']").setAttribute("type", "password");
-}
+    function maskowanie (e) {
+        document.querySelector("input[name='imie']").setAttribute("type", "password");
+    }
 
 
-var elementyFormularza = document.querySelectorAll("form input");
-console.log("odnaleziono: " + elementyFormularza.length + " elementow input");
+    var elementyFormularza = document.querySelectorAll("form input");
+    console.log("odnaleziono: " + elementyFormularza.length + " elementow input");
 
-var przyciskPow = document.querySelector("#powiekszenie");
-przyciskPow.addEventListener("click", powiekszenie);
+    var przyciskPow = document.querySelector("#powiekszenie");
+    przyciskPow.addEventListener("click", powiekszenie);
 
-var przyciskPom = document.querySelector("#pomniejszenie");
-przyciskPom.addEventListener("click", pomniejszenie);
+    var przyciskPom = document.querySelector("#pomniejszenie");
+    przyciskPom.addEventListener("click", pomniejszenie);
 
-document.querySelector("#maskuj").addEventListener("click", maskowanie);
+    document.querySelector("#maskuj").addEventListener("click", maskowanie);
+
+})();
